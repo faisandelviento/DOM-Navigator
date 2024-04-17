93ec69bdece2e7d8c5fed8c794153d050c05d53e
faisandelviento <cristinaclaverluna@hotmail.com>
2024-04-17 20:58:40 +0200

    coemntarios

--- domnavigator.js
+++ domnavigator.js
@@ -1,16 +1,9 @@
-// Constantes globales
-const anchoTablero = 8;
-const altoTablero = 8;
+//Cristina Claver
 
-//como hacer la ruta
-//control de teclado
-//key.down  prevent default para que no baje la pantalla con la barra, tambien existe el stoppropagation()(para evetar la efervescencia)
-
-// Método window.onload
-// Añadir aquí solamente:
 //  1.Registro de listeners de eventos del DOM
 window.onload = function () {
     const casillas = document.querySelectorAll(".casilla"); 
+    //añadir evento a cada casilla
     casillas.forEach(element => {
         element.addEventListener("click", TurnRed)
     });
@@ -18,7 +11,9 @@ window.onload = function () {
 
 function TurnRed(e){
     const casillaSelect = e.target;
+    //cada vez que se clica una casilla, se borra el color rojo
     BorrarTodo();
+    //consicionales para escojer el modo
     if (document.querySelector('input[id="modoCasilla"]').checked){
         casillaSelect.classList.add("casillaSel");
         ChangePath(casillaSelect);
@@ -31,6 +26,8 @@ function TurnRed(e){
     } else if(document.querySelector('input[id="modoCol"]').checked) {
         let indice = Array.from(casillaSelect.parentElement.children).indexOf(casillaSelect);
         const allLineas = document.querySelectorAll(".linea")
+        //se pintan de cada class 'linea' solo los hijos que coincide
+        // con el indice de la casilla qeu se ha clicado
         allLineas.forEach(linea => {
             linea.children[indice].classList.add("casillaSel");             
         });
@@ -48,6 +45,7 @@ function ChangePath(elemento) {
 
 function BorrarTodo() {
     const casillasRojas = document.getElementsByClassName("casillaSel");
+    //utilizo while en vez de foreach pq es un htmlelement
     while (casillasRojas.length > 0) {
         casillasRojas[0].classList.remove("casillaSel");
     }
